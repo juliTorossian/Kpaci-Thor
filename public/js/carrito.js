98ba16bd9577e552c436a827285712ef152f9571
juliTorossian <julian.torossian@outlook.com>
2021-06-11 16:00:53 -0300

    .gitignore | correccion cantidades y costos en el carrito
    En Carrito ya se puede modificar la cantidad. Falta agregar modificacion de db o de json
    
    agregue un .gitignore para que ignore todo archivo que tenga '-ign'

---
+++ public/js/carrito.js
@@ -0,0 +1,60 @@
+
+const sCant    = document.querySelectorAll('#sCant')
+const preTot   = document.querySelectorAll('#preTotPrd')
+const preInd   = document.querySelectorAll('#preIndPrd')
+const preTotal = document.querySelector('#preTotal')
+
+var cantidad = parseInt(sCant.innerHTML,2);
+//console.log(cantidad);
+
+//bSumar.addEventListener('click', sumar);
+//bRestar.addEventListener('click', restar);
+
+function sumar(pos){
+    var cantidad = parseInt(sCant[pos].innerHTML);
+    cantidad += 1;
+    sCant[pos].innerHTML = cantidad.toString();
+    //console.log(cantidad);
+    //console.log('sumar');
+    actualizarPrecio(cantidad, pos);
+}
+
+function restar(pos){
+    var cantidad = parseInt(sCant[pos].innerHTML);
+    if (cantidad > 1){
+        cantidad -= 1;
+        sCant[pos].innerHTML = cantidad.toString();
+        //console.log(cantidad);
+        //console.log('restar');
+        actualizarPrecio(cantidad, pos);
+    }
+}
+
+function actualizarPrecio(cantidad, pos){
+
+    var aux = preInd[pos].innerHTML.split(' ');
+    var precioInd = parseFloat(aux[1]);
+    precioAct = (precioInd * cantidad).toFixed(2);
+
+    preTot[pos].textContent = aux[0] +' ' +precioAct.toString();
+
+    actualizaTotal();
+}
+
+
+function actualizaTotal(){
+    //const preTotSec  = document.querySelectorAll('#preTotPrd')
+    
+    var precioTotal = 0;
+    var count = 0;
+    var aux;
+    while(count < preTot.length){
+        aux = preTot[count].innerHTML.split(' ');  
+        //console.log(aux);
+        precioTotal += parseFloat(aux[1]);
+        count++;
+    }
+
+    //console.log(aux[0] +' ' +precioTotal);
+    preTotal.textContent = aux[0] +' ' +precioTotal;
+}
